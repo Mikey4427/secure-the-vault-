6de612fc8b61dd0e5a5287b7f5a21a7ff481cc34
michael <michael.gau27@gmail.com>
2023-10-23 18:51:39 -0400

    probably lat commit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-//set text for display cobo
+//set text for display cobmbo
 const text = ` You have received this message because 
 you have been chosen to open an important vault.
  Here is the secret combination:`;
  document.write(text);
 // set variable for 3 numbers using arithmatic 
 //these variables will be the safe combo
-let comboOne = 10 * 3;
-let comboTwo = 26 / 2;
-const comboThree = 5 + 6;
+// each variable will have a random intiger tha changes whenever the page is refreshed 
+let comboOne = Math.floor(Math.random() * 34) * 3;
+let comboTwo = Math.floor(Math.random() * 101) / 1;
+const comboThree = Math.floor(Math.random() * 94) + 6;
 // creat alert that displays code 
 // creat button to get code
 // creat function to add utility to the button 
