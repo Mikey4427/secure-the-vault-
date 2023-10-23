232fdb5a19cd4c6cca16727b2e6a09836d4ed0e1
michael <michael.gau27@gmail.com>
2023-10-23 18:17:15 -0400

    second commit

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 const text = ` You have received this message because 
 you have been chosen to open an important vault.
  Here is the secret combination:`;
+ document.write(text);
 // set variable for 3 numbers using arithmatic 
+//these variables will be the safe combo
 let comboOne = 10 * 3;
 let comboTwo = 26 / 2;
 const comboThree = 5 + 6;
 // creat alert that displays code 
-alert(`${comboOne} ${comboTwo} ${comboThree}`);
+// creat button to get code
+// creat function to add utility to the button 
+function getCombo() {
+    alert(`${comboOne} ${comboTwo} ${comboThree}`);
+}
