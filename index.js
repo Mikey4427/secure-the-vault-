0b2dc69a4c2f80e31b71bb6e59ea4d52fe53455b
michael <michael.gau27@gmail.com>
2023-10-23 16:14:22 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//set text for display cobo
+const text = ` You have received this message because 
+you have been chosen to open an important vault.
+ Here is the secret combination:`;
+// set variable for 3 numbers using arithmatic 
+let comboOne = 10 * 3;
+let comboTwo = 26 / 2;
+const comboThree = 5 + 6;
+// creat alert that displays code 
+alert(`${comboOne} ${comboTwo} ${comboThree}`);
